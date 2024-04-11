dd7a8d7910c3f749244921d44563a919e90cac93
bgarlock <bgarlock@bgarlocks-mbp.myfiosgateway.com>
2024-04-11 16:10:01 -0400

    Add input variable

--- api/functionapp/helloworld/src/main/java/helloworld/HelloWorldFunction.java
+++ api/functionapp/helloworld/src/main/java/helloworld/HelloWorldFunction.java
@@ -24,14 +24,22 @@ public class HelloWorldFunction {
      */
     @FunctionName("/helloworld")
     public HttpResponseMessage run(
-            @HttpTrigger(
-                    name = "request",
-                    methods = {HttpMethod.GET},
-                    authLevel = AuthorizationLevel.ANONYMOUS
-            ) HttpRequestMessage<Optional<String>> request,
+             @HttpTrigger(
+                name = "req",
+                methods = {HttpMethod.GET, HttpMethod.POST},
+                authLevel = AuthorizationLevel.ANONYMOUS)
+                HttpRequestMessage<Optional<String>> request,
             final ExecutionContext context) {
-        return request.createResponseBuilder(HttpStatus.OK).
-                body("Hello World").
-                build();
+        context.getLogger().info("Java HTTP trigger processed a request.");
+
+        // Parse query parameter
+        final String query = request.getQueryParameters().get("name");
+        final String name = request.getBody().orElse(query);
+
+        if (name == null) {
+            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
+        } else {
+            return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + name).build();
+        }
     }
 }
